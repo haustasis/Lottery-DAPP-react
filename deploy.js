f2b14a01f0a614b429320ce92f8b2ef0d7629a80
TARISO <toni.ariso@gmail.com>
2021-02-15 13:53:22 +0100

    Deploying to Rinkeby

--- deploy.js
+++ deploy.js
@@ -8,4 +8,10 @@ const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: '0x' + bytecode })
+    .send({ gas: '2,000,000' });
 };
+
+deploy();
