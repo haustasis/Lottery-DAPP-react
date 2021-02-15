82fffdfaa51b3750973267e2a6a939e545d3cf43
TARISO <toni.ariso@gmail.com>
2021-02-15 13:10:09 +0100

    Seeting Up Pages With Next

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Lottery extends Component {
+  render() {
+    return <h1> Decentralized Lottery Application </h1>;
+  }
+}
+
+export default Lottery;
